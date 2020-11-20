c0878007010620d4ceab1dbd3e3ccd7a26066c7e
trile2k <70605945+trile2k@users.noreply.github.com>
2020-11-20 02:00:16 -0600

    Implemented map features - marker, info message, tool bar

--- js/script.js
+++ js/script.js
@@ -1,47 +1,93 @@
+// Project 3 JavaScript for Google Map.
+// Three Features Implemented:
+//   1. Gold Star Marker.
+//   2. Tool Bar to place Marker, Circle, Rectangle, etc on the Map.
+//   3. Pop Up Info Message with Title when hovering over the Marker.
 
-
-// function initMap() {
-//   alert('it works');
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: -34.397, lng: 150.644},
-//     zoom: 8
-//   });
-// }
-
+// Map Initialization.
 function init() {
 	// alert('it works');
-	var el = document.getElementById('canvas');
-	// var myLocation = new google.maps.LatLng(41.835117, -87.627130);
-  var myLocation = new google.maps.LatLng(10.779790, 106.699012);
+  // Set up the Map with various options.
+	var canvas_element = document.getElementById('canvas');
+  var myLocation = new google.maps.LatLng(10.779790, 106.699012); // Ho Chi Minh City
 	var mapOptions = {
 		center: myLocation,
-		zoom: 14,
-		mapTypeId: google.maps.MapTypeId.SATELLITE,
+		zoom: 13,
+		mapTypeId: 'hybrid',
+    mapTypeControl: true,
 		mapTypeControlOptions: {
-			position: google.maps.ControlPosition.BOTTOM_CENTER
-		}
+			position: google.maps.ControlPosition.BOTTOM_CENTER,
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+      mapTypeIds: ["roadmap", "terrain"],
+		},
 	};
 
-	var myMap = new google.maps.Map(el, mapOptions);
+  // Form a goldstar for the map
+  const goldStar = {
+    path:
+      "M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z",
+    fillColor: "yellow",
+    fillOpacity: 0.8,
+    scale: 0.15,
+    strokeColor: "gold",
+    strokeWeight: 14,
+  };
 
+  // Create the map
+  var myMap = new google.maps.Map(canvas_element, mapOptions);
+
+  // Create the marker on the map
 	var marker = new google.maps.Marker({
 		position: myLocation,
-		map: myMap
-		// animation: google.maps.Animation.BOUNCE,
-		// icon: 'iit-icon.png'
+    title: "Ho Chi Minh City (Saigon)",
+    icon: goldStar, // Use the Goldstar created earlier
+		map: myMap,
 	});
 
-	var contentString = '<h1>Ho Chi Minh City</h1><p>It is the largest city situated in \
+  // Create a string to write info message to the map window
+	var contentString = '<h1 id="h1map">Ho Chi Minh City</h1><p id="pmap">It is the largest city situated in \
     the South of Vietnam and is also known as Saigon.</p>';
 
 	var infowindow = new google.maps.InfoWindow({
-      content: contentString
+      content: contentString,
+      maxWidth: 200, // Limit the message size so it's not too big.
 	});
 
+  // Write an info messgae when mouse over on the marker.
 	google.maps.event.addListener(marker, 'mouseover', function() {
     	infowindow.open(myMap, marker);
 	});
 
+  // Enable Drawing Tool at the top of the Map.
+  const drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [
+        google.maps.drawing.OverlayType.MARKER,
+        google.maps.drawing.OverlayType.CIRCLE,
+        google.maps.drawing.OverlayType.POLYGON,
+        google.maps.drawing.OverlayType.POLYLINE,
+        google.maps.drawing.OverlayType.RECTANGLE,
+      ],
+    },
+    markerOptions: {
+      icon:
+        "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+    },
+    circleOptions: {
+      fillColor: "#ffff00",
+      fillOpacity: 1,
+      strokeWeight: 5,
+      clickable: false,
+      editable: true,
+      zIndex: 1,
+    },
+  });
+
+  drawingManager.setMap(myMap); // Create the drawing Tool at the top of the Map.
+  myMap.setTilt(45);
 }
 
-google.maps.event.addDomListener(window, 'load', init);
+// google.maps.event.addDomListener(window, 'load', init);
